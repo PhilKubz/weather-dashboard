dad3e1c2b49d8c44124eb3c5de177cb2827b5dd5
PhilKubsz <philip.kubisz@gmail.com>
2023-03-07 21:51:24 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const apiForecast = 'https://api.openweathermap.org/data/2.5/forecast/daily'
+
+
+/* Future Use
+weatherItem.innerHTML = `
+<div class="WeatherListBox">
+<p>${cityName}</p>
+<p>Date: ${date}</p>
+<p>Temperature: ${temperature}F</p>
+<p>Wind speed: ${wind} m/s</p>
+<p>Humidity: ${humidity}%</p>
+</div>
+`;
+
+*/
