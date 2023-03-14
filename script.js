1a0747b47b9b4d98bd9e5a38e91404ba7a363e54
PhilKubsz <philip.kubisz@gmail.com>
2023-03-14 10:49:48 -0700

    project first steps UI

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-const apiForecast = 'https://api.openweathermap.org/data/2.5/forecast/daily'
+const API_Key =  "2bbcb13c772b86880faaf56560555d8f";
 
+const Weather_API_URL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=" + API_Key;
+
+
+// Weather API Functions
+
+fetch(WEATHER_API_URL)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+
+
+// City Form Functions
+
+function fillCity(cityName) {
+    document.getElementById('city-input').value = cityName;
+    document.getElementById('city-form').submit();
+  }
 
 /* Future Use
 weatherItem.innerHTML = `
