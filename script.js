9765c4997f4915ac4072f9b37f61ae7dc73f82a3
Phil <philip.kubisz@gmail.com>
2023-06-17 17:22:31 -0700

    functionality/UI improvements

--- script.js
+++ script.js
@@ -1,37 +1,130 @@
-const API_Key =  "2bbcb13c772b86880faaf56560555d8f";
-
-const Weather_API_URL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=" + API_Key;
-
+const API_KEY = "2bbcb13c772b86880faaf56560555d8f";
 
 // Weather API Functions
+function fetchWeatherData(city) {
+  const weatherAPIURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
 
-fetch(WEATHER_API_URL)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error(error);
+  fetch(weatherAPIURL)
+    .then(response => response.json())
+    .then(data => {
+      displayCurrentWeather(data);
+      fetchForecastData(data.coord.lat, data.coord.lon);
+      storeSearchHistory(city);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+function fetchForecastData(lat, lon) {
+  const forecastAPIURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly&appid=${API_KEY}`;
+
+  fetch(forecastAPIURL)
+    .then(response => response.json())
+    .then(data => {
+      displayForecast(data.daily);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+function displayCurrentWeather(data) {
+  const currentWeatherContainer = document.getElementById('current-weather');
+  currentWeatherContainer.innerHTML = '';
+
+  const cityName = data.name;
+  const date = new Date().toLocaleDateString();
+  const icon = data.weather[0].icon;
+  const temperatureCelsius = Math.round(data.main.temp - 273.15); // Temperature in Celsius
+  const temperatureFahrenheit = Math.round((temperatureCelsius * 9) / 5 + 32); // Temperature in Fahrenheit
+  const humidity = data.main.humidity;
+  const windSpeed = data.wind.speed;
+
+  const currentWeatherHTML = `
+    <div class="weather-item">
+      <h3>${cityName}</h3>
+      <p>Date: ${date}</p>
+      <p>Temperature: ${temperatureCelsius}°C / ${temperatureFahrenheit}°F</p>
+      <p>Humidity: ${humidity}%</p>
+      <p>Wind Speed: ${windSpeed} m/s</p>
+    </div>
+  `;
+
+  currentWeatherContainer.innerHTML = currentWeatherHTML;
+}
+
+function displayForecast(forecastData) {
+  const forecastContainer = document.getElementById('forecast');
+  forecastContainer.innerHTML = '';
+
+  forecastData.forEach(day => {
+    const date = new Date(day.dt * 1000).toLocaleDateString();
+    const icon = day.weather[0].icon;
+    const temperatureCelsius = Math.round(day.temp.day - 273.15); // Temperature in Celsius
+    const temperatureFahrenheit = Math.round((temperatureCelsius * 9) / 5 + 32); // Temperature in Fahrenheit
+    const humidity = day.humidity;
+    const windSpeed = day.wind_speed;
+
+    const forecastItemHTML = `
+      <div class="weather-item">
+        <h3>${date}</h3>
+        <p>Temperature: ${temperatureCelsius}°C / ${temperatureFahrenheit}°F</p>
+        <p>Humidity: ${humidity}%</p>
+        <p>Wind Speed: ${windSpeed} m/s</p>
+      </div>
+    `;
+
+    forecastContainer.innerHTML += forecastItemHTML;
   });
+}
 
+function storeSearchHistory(city) {
+  let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
 
-
-// City Form Functions
-
-function fillCity(cityName) {
-    document.getElementById('city-input').value = cityName;
-    document.getElementById('city-form').submit();
+  // Avoid duplicates in search history
+  if (!searchHistory.includes(city)) {
+    searchHistory.push(city);
+    localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
   }
 
-/* Future Use
-weatherItem.innerHTML = `
-<div class="WeatherListBox">
-<p>${cityName}</p>
-<p>Date: ${date}</p>
-<p>Temperature: ${temperature}F</p>
-<p>Wind speed: ${wind} m/s</p>
-<p>Humidity: ${humidity}%</p>
-</div>
-`;
+  displaySearchHistory(searchHistory);
+}
 
-*/
+function displaySearchHistory(searchHistory) {
+  const searchHistoryList = document.getElementById('search-history-list');
+  searchHistoryList.innerHTML = '';
+
+  searchHistory.forEach(city => {
+    const searchHistoryItem = document.createElement('button');
+    searchHistoryItem.className = 'search-history-button';
+    searchHistoryItem.textContent = city;
+    searchHistoryItem.addEventListener('click', () => {
+      fetchWeatherData(city);
+    });
+
+    searchHistoryList.appendChild(searchHistoryItem);
+  });
+}
+
+function fillCity(city) {
+  const cityInput = document.getElementById('city-input');
+  cityInput.value = city;
+}
+
+// Event Listeners
+document.getElementById('city-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  const cityInput = document.getElementById('city-input');
+  const city = cityInput.value;
+  fetchWeatherData(city);
+});
+
+// Initial Load
+const lastSearchedCity = localStorage.getItem('lastSearchedCity');
+if (lastSearchedCity) {
+  fetchWeatherData(lastSearchedCity);
+}
+
+const searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+displaySearchHistory(searchHistory);
